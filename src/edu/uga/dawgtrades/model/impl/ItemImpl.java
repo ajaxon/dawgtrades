2d54a61727983c50b4065949c0216fa68333623c
mattlisiv <lisivickmatt@gmail.com>
2014-11-14 14:46:35 -0500

    ItemImpl-Update

--- src/edu/uga/dawgtrades/model/impl/ItemImpl.java
+++ src/edu/uga/dawgtrades/model/impl/ItemImpl.java
@@ -5,79 +5,119 @@ import edu.uga.dawgtrades.model.Item;
 import edu.uga.dawgtrades.model.RegisteredUser;
 
 public class ItemImpl extends Persistent implements Item {
-
+		
+		private Category category;
+		private RegisteredUser user;
+		private String identifier;
+		private String name;
+		private String description;
+		private long userId;
+		private long categoryId;
+		
 	public ItemImpl(Category category, RegisteredUser user, String identifier,
 			String name, String description) {
+			
+			super(-1);
+			this.setCategory(category);
+			this.categoryId=category.getId();
+			this.setUser(user);
+			this.userId=user.getId();
+			this.identifier=identifier;
+			this.name=name;
+			this.description=description;
+		
 
 	
 	}
 	
-	public ItemImpl(Category category, long userID, String identifier,
+	public ItemImpl(long categoryId, long userId, String identifier,
 			String name, String description) {
+			super(-1);
+			this.categoryId=categoryId;
+			this.userId=userId;
+			this.identifier=identifier;
+			this.name=name;
+			this.description=description;
 
-	
 	}
 	
 	
 
 	@Override
 	public String getIdentifier() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.identifier;
 	}
 
 	@Override
 	public void setIdentifier(String identifier) {
-		// TODO Auto-generated method stub
 
+		this.identifier=identifier;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.name;
 	}
 
 	@Override
 	public void setName(String name) {
-		// TODO Auto-generated method stub
 
+		this.name=name;
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.description;
 	}
 
 	@Override
 	public void setDescription(String description) {
-		// TODO Auto-generated method stub
 
+		this.description=description;
 	}
 
 	@Override
 	public long getCategoryId() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return this.categoryId;
 	}
 
 	@Override
 	public void setCategoryId(long categoryId) {
-		// TODO Auto-generated method stub
 
+		this.categoryId=categoryId;
 	}
 
 	@Override
 	public long getOwnerId() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		
+		return this.userId;
 	}
 
 	@Override
 	public void setOwnerId(long ownerId) {
-		// TODO Auto-generated method stub
 
+		this.userId=ownerId;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public RegisteredUser getUser() {
+		return user;
+	}
+
+	public void setUser(RegisteredUser user) {
+		this.user = user;
 	}
 
 }
