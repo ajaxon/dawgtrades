ac360117980df268e502cd02d6ab6eff64cfebca
Allen <jaxon77@gmail.com>
2014-11-15 17:14:43 -0500

    db config

--- src/edu/uga/dawgtrades/persist/impl/DbAccessConfig.java
+++ src/edu/uga/dawgtrades/persist/impl/DbAccessConfig.java
@@ -1,8 +1,30 @@
 package edu.uga.dawgtrades.persist.impl;
 
-public class DbAccessConfig {
 
-	
-	//Needs to be implemented
-	
+public class DbAccessConfig
+{
+    /** The fully qualified name of the JDBC driver.
+     */
+    static final String DB_DRIVE_NAME  = "com.mysql.jdbc.Driver";
+
+    /** The database name
+     */
+    static final String DB_NAME        = "team2";
+
+    /** The database server name for the connection pool
+     */
+    static final String DB_SERVER_NAME = "localhost";
+
+    /** The JDBC connection string/URL.
+     */
+    static final String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/team2";
+
+    /** The database user name.
+     */
+    static  String DB_CONNECTION_USERNAME = "team2";
+
+    /** The password for the database user.
+     */
+    static  String DB_CONNECTION_PWD = "interface";
+
 }
