4ffe1d5425418bdc90c87b0dd86c7a78a60235bb
dtdaniel <dtdaniel@uga.edu>
2014-11-14 23:37:13 -0500

    Update AttributeImpl.java
    implementation of attribute

--- src/edu/uga/dawgtrades/model/impl/AttributeImpl.java
+++ src/edu/uga/dawgtrades/model/impl/AttributeImpl.java
@@ -5,67 +5,60 @@ import edu.uga.dawgtrades.model.AttributeType;
 import edu.uga.dawgtrades.model.Item;
 
 public class AttributeImpl extends Persistent implements Attribute {
+	
+	private long attributeTypeId;
+	private long itemId;
+	private String value;
 
 	public AttributeImpl(AttributeType attributeType, Item item, String value) {
-		// TODO Auto-generated constructor stub
+		super(-1);
+		if(attributeType == null)
+			throw new DTException("Attribute Type is null");
+		if(!attributeType.isPersistent())
+			throw new DTException("Attribute Type is not persistent");
+		if(item == null)
+			throw new DTException("Item is null");
+		if(!item.isPersistent())
+			throw new DTException("Item is not persistent");
+		this.attributeTypeId = attributeType.getId();
+		this.itemId = item.getId();
+		this.value = value;
 	}
 
 	public AttributeImpl(long attributeTypeId, long itemId, String value) {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public long getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setId(long id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isPersistent() {
-		// TODO Auto-generated method stub
-		return false;
+		this.attributeTypeId = attributeTypeId;
+		this.itemId = itemId;
+		this.value = value;
 	}
 
 	@Override
 	public String getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
 
 	@Override
 	public void setValue(String value) {
-		// TODO Auto-generated method stub
-
+		this.value = value;
 	}
 
 	@Override
 	public long getItemId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return itemId;
 	}
 
 	@Override
 	public void setItemId(long itemId) {
-		// TODO Auto-generated method stub
-
+		this.itemId = itemId;
 	}
 
 	@Override
 	public long getAttributeType() {
-		// TODO Auto-generated method stub
-		return 0;
+		return attributeTypeId;
 	}
 
 	@Override
 	public void setAttributeType(long attributeId) {
-		// TODO Auto-generated method stub
-
+		this.attributeTypeId = attributeId;
 	}
 
 }
