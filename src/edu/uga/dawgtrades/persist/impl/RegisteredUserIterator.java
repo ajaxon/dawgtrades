411d47bf9058c067da113ba2f9345b1b2cab0cc4
dtdaniel <dtdaniel@uga.edu>
2014-11-16 16:20:04 -0500

    Update RegisteredUserIterator.java
    implementing next

--- src/edu/uga/dawgtrades/persist/impl/RegisteredUserIterator.java
+++ src/edu/uga/dawgtrades/persist/impl/RegisteredUserIterator.java
@@ -29,10 +29,49 @@ public class RegisteredUserIterator implements Iterator<RegisteredUser> {
 		return more;
 	}
 
+
 	@Override
-	public RegisteredUser next() {
-		// TODO Auto-generated method stub
-		return null;
+	public RegisteredUser next() throws DTException {
+
+		    long   id;
+		    String name;
+		    String firstName;
+		    String lastName;
+		    String password;
+		    String email;
+		    String phone;
+		    boolean canText;
+		    boolean isAdmin;
+		
+		    if( more ) {
+		
+		        try {
+		            id = rs.getLong( 1 );
+		            name = rs.getString( 2 );
+		            firstName = rs.getString( 3 );
+		            lastName = rs.getString( 4 );
+		            password = rs.getString( 5 );
+		            email = rs.getString( 6 );
+		            phone = rs.getString( 7 );
+		            canText = rs.getBoolean( 8 );
+		            isAdmin = rs.getBoolean( 9 );
+		
+		            more = rs.next();
+		        }
+		        catch( Exception e ) {	// just in case...
+		            throw new DTException( "RegisteredUserIterator: No next RegisteredUser object; root cause: " + e );
+		        }
+		        
+		        RegisteredUser registeredUser = objectModel.createRegisteredUser( name, firstName, lastName, password, email, phone, canText, isAdmin );
+		        registeredUser.setId( id );
+		        
+		        return registeredUser;
+		    }
+		    else {
+		        throw new DTException( "RegisteredUserIterator: No next RegisteredUser object" );
+		    }
+		}
+
 	}
 
 	@Override
