1d84a6e018e26419b3d71558d7ea969d72372b42
Allen <jaxon77@gmail.com>
2014-12-02 18:24:21 -0500

    Register servlet

--- WEB-INF/templates/register.ftl
+++ WEB-INF/templates/register.ftl
@@ -4,5 +4,15 @@
 </head>
 <body>
 ${message}
+
+<form method=post name="Register" action="register" style="margin:0;padding:0;">
+    <input type="text" name="name" value="">
+    <input type="text" name="firstName" value="">
+    <input type="text" name="lastName" value="">
+    <input type="password" name="password" value="">
+    <input type="text" name="email" value="">
+    <input type="checkbox" name="canText" value="off">
+    <input type="submit" name="Register" value="submit">
+    </form>
 </body>
 </html>
--- src/edu/uga/dawgtrades/servlets/Register.java
+++ src/edu/uga/dawgtrades/servlets/Register.java
@@ -1,16 +1,118 @@
 package edu.uga.dawgtrades.servlets;
 
+import javax.security.auth.login.Configuration;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.Writer;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import freemarker.template.*;
+
+import edu.uga.dawgtrades.model.DTException;
+import edu.uga.dawgtrades.model.ObjectModel;
+import edu.uga.dawgtrades.model.RegisteredUser;
+import edu.uga.dawgtrades.model.impl.ObjectModelImpl;
+import edu.uga.dawgtrades.persist.Persistence;
+import edu.uga.dawgtrades.persist.impl.DbUtils;
+import edu.uga.dawgtrades.persist.impl.PersistenceImpl;
 
 /**
  * Created by Allen on 11/27/14.
  */
 public class Register extends javax.servlet.http.HttpServlet {
+    static Connection conn = null;
+    static ObjectModel objectModel = null;
+    static Persistence persistence = null;
+    private freemarker.template.Configuration cfg;
+    // get a database connection
+
+
+    public void init() {
+        // Initialize the FreeMarker configuration;
+        // - Create a configuration instance
+        cfg = new freemarker.template.Configuration();
+        // - Templates are stored in the WEB-INF/templates directory of the Web app.
+        cfg.setServletContextForTemplateLoading(
+                getServletContext(), "WEB-INF/templates");
+
+    }
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
+        getConnection();
+        RegisteredUser user = objectModel.createRegisteredUser();
+        user.setName(request.getParameter("name"));
+        user.setFirstName(request.getParameter("firstName"));
+        user.setLastName(request.getParameter("lastName"));
+        user.setEmail(request.getParameter("email"));
+        user.setCanText(Boolean.parseBoolean(request.getParameter("canText")));
+        user.setPassword(request.getParameter("password"));
+        user.setPhone(request.getParameter("phone"));
+
+        try {
+            objectModel.storeRegisteredUser(user);
+        } catch (DTException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
+        // Prepare template input
+        Map<String, Object> root = new HashMap<String, Object>();
+        root.put("message","testing this shit ");
+        // Get the templat object
+        Template template = cfg.getTemplate("register.ftl");
+
+        // Prepare the HTTP response:
+        // - Use the charset of template for the output
+        // - Use text/html MIME-type
+        response.setContentType("text/html; charset=" + template.getEncoding());
+        Writer out = response.getWriter();
+
+        // Merge the data-model and the template
+        try {
+            template.process(root, out);
+        } catch (TemplateException e) {
+            throw new ServletException(
+                    "Error while processing FreeMarker template", e);
+        }
+
+    }
+
+
+    public void getConnection()
+    {
+        try {
+            conn = DbUtils.connect();
+
+        }
+        catch (Exception seq) {
+            System.err.println( "ObjectModelDelete: Unable to obtain a database connection" );
+        }
+
+        // obtain a reference to the ObjectModel module
+        objectModel = new ObjectModelImpl();
+
+        // obtain a reference to Persistence module and connect it to the ObjectModel
+        persistence = new PersistenceImpl( conn, objectModel );
+
+        // connect the ObjectModel module to the Persistence module
+        objectModel.setPersistence( persistence );
+    }
+    public void disconnect() throws SQLException {
+        conn.close();
     }
 }
