da17ea3f36efa8cec25c29b0333d82b81154b42f
Joel Scott <toasteroverheated@gmail.com>
2014-12-08 11:50:01 -0500

    Update Category still a bit broken

--- src/edu/uga/dawgtrades/servlets/ViewProfile.java
+++ src/edu/uga/dawgtrades/servlets/ViewProfile.java
@@ -1,23 +1,26 @@
 package edu.uga.dawgtrades.servlets;
-
 import edu.uga.dawgtrades.authentication.Session;
 import edu.uga.dawgtrades.authentication.SessionManager;
 import edu.uga.dawgtrades.model.RegisteredUser;
+import edu.uga.dawgtrades.persist.Persistence;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.Connection;
 
 /**
  * Created by Allen on 11/27/14.
  */
 public class ViewProfile extends javax.servlet.http.HttpServlet {
 
+    static Connection conn = null;
+    static Persistence persistence = null;
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         HttpSession httpSession = null;
         String ssid = null;
         Session session = null;
         RegisteredUser user = null;
-
         httpSession = request.getSession();
         if (httpSession.getAttribute("ssid") != null) {
             ssid = (String) httpSession.getAttribute("ssid");
@@ -27,7 +30,23 @@ public class ViewProfile extends javax.servlet.http.HttpServlet {
                 System.out.println("No session found");
             } else {
                 user = session.getUser();
-
+                user.setName(request.getParameter("username"));
+                user.setPassword(request.getParameter("password"));
+                user.setFirstName(request.getParameter("firstName"));
+                user.setLastName(request.getParameter("lastName"));
+                user.setEmail(request.getParameter("email"));
+                user.setPhone(request.getParameter("phone"));
+                String text = request.getParameter("canText");
+                if(text == "false"){
+                    user.setCanText(false);
+                }else{
+                    user.setCanText(true);
+                }
+//                try {
+//                    persistence.saveRegisteredUser(user);
+//                } catch (DTException e) {
+//                    e.printStackTrace();
+//                }
             }
             request.getRequestDispatcher("view_profile.ftl").forward(request,response);
         }
@@ -50,9 +69,9 @@ public class ViewProfile extends javax.servlet.http.HttpServlet {
                 user = session.getUser();
                 request.setAttribute("user", user);
                 if(user.getCanText() == false){
-                    request.setAttribute("text", "No");
+                    request.setAttribute("canText", false);
                 }else{
-                    request.setAttribute("text", "Yes");
+                    request.setAttribute("canText", true);
                 }
                 request.getRequestDispatcher("view_profile.ftl").forward(request,response);
             }
--- templates/view_profile.ftl
+++ templates/view_profile.ftl
@@ -4,21 +4,19 @@
 </head>
 <body>
     <h4>View Profile</h4>
-    <form action="view_profile">
+    <form method="post" action="view_profile">
         <br>Name: <input type="text" name="username" value="${user.name}">
         <br>Password: <input type="text" name="password" value="${user.password}">
         <br>First Name: <input type="text" name="firstName" value="${user.firstName}">
         <br>Last Name: <input type="text" name="lastName" value="${user.lastName}">
         <br>Email: <input type="text" name="email" value="${user.email}">
         <br>Phone: <input type="text" name="phone" value="${user.phone}">
-        <br>Text: <input type="text" name="text" value="${text}">
+        <br>Text: <input type="checkbox" name="canText" value="canText">
         <br>
+        <input type="submit" value="Update">
     </form>
     <form action="login">
         <input type="submit" value="Back to Index">
-    </form>
-    <form action="view_profile">
-        <input type="submit" value="Update">
     </form>
 </body>
 </html>
